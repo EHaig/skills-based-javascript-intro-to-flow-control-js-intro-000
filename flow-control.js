d4e597a9f7d6c3556f1af1b6d102e7586d584455
EHaig <info@elianahaig.com>
2017-10-12 15:57:36 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -27,7 +27,7 @@ function ternaryTeenager(age) {
 return age >= 13 && age <= 19 ? "You are a teenager": "You are not a teenager"
 }
 
-function switchAge() {
+function switchAge(age) {
   switch (age) {
   case age = 13:
   return "You are a teenager"
